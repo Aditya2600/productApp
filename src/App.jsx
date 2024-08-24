79adbcd722c9dccb8bc8370ba6e690c03336ff84
Aditya <126133127+Aditya2600@users.noreply.github.com>
2024-08-25 00:27:49 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./component/Home";
+import Details from "./component/Details";
+import Create from "./component/Create";
+import Edit from "./component/Edit";
+
+const App = () => {
+  const { search, pathname } = useLocation();
+  return (
+    <div className="h-screen w-screen flex">
+      {(pathname != "/" || search.length > 0) && (
+        <Link to="/" className="text-red-300 absolute left-[17%] top-[3%] ">
+          Home
+        </Link>
+      )}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/edit/:id" element={<Edit  />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
